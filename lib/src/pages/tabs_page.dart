f5a8cb9892ea13435889adf8f1e54ebc520b3492
Emmanuel Hernández Maqueda <emhermaq@gmail.com>
2021-09-26 20:40:01 -0500

    Iniciando Aplicación de Noticias

---
+++ lib/src/pages/tabs_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class TabsPage extends StatelessWidget {
+  const TabsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => _NavegacionModel(),
+      child: Scaffold(
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+      ),
+    );
+  }
+}
+
+class _Navegacion extends StatelessWidget {
+  const _Navegacion({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
+    return BottomNavigationBar(
+      currentIndex: navegacionModel.paginaActual,
+      onTap: (i) => navegacionModel.paginaActual = i,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          title: Text('Para ti'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.public),
+          title: Text('Encabezados'),
+        ),
+      ],
+    );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  const _Paginas({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
+    return PageView(
+      controller: navegacionModel.pageController,
+      // physics: const BouncingScrollPhysics(),
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Container(color: Colors.red),
+        Container(color: Colors.green),
+      ],
+    );
+  }
+}
+
+class _NavegacionModel with ChangeNotifier {
+  int _paginaActual = 0;
+  PageController _pageController = PageController();
+
+  int get paginaActual => _paginaActual;
+
+  set paginaActual(int valor) {
+    // Cambiar el menu
+    _paginaActual = valor;
+    // Cambiar el PageView
+    _pageController.animateToPage(valor,
+        duration: const Duration(milliseconds: 250), curve: Curves.easeInOut);
+    notifyListeners();
+  }
+
+  PageController get pageController => _pageController;
+}
